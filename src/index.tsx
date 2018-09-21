1eb2dbe7ab9120e99ff5375d1833b5fde3b7480b
Leonid <leonidyarmak@gmail.com>
2018-09-21 23:14:05 +0300

    Added why-did-you-update plugin

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,15 @@ import 'core-js'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import './scss/main.scss'
+import { whyDidYouUpdate } from 'why-did-you-update'
 
 import AppStore from './store/store'
 import App from './App'
 import { AppContainer } from 'react-hot-loader'
+
+if (process.env.APP_ENV !== 'production') {
+  whyDidYouUpdate(React)
+}
 
 ReactDOM.render(
   <AppContainer>
